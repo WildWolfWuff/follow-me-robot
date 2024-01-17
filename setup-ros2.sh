3c899f1eb7baf4759c5e4fe360d5be7fbf5cba93
Marc Hummel <mhummel@wildwolfwuff.de>
2024-01-17 17:16:16 +0100

    add sudo to commands

--- setup-ros2.sh
+++ setup-ros2.sh
@@ -5,7 +5,7 @@ printError(){
 printStep() {
     echo -e "STEP: \e[32m${1}\e[0m"
 }
-[ `whoami` = root ] || { printError "Run with as root or sudo"; exit 1; }
+
 distro=iron
 while getopts ":d:e" opt; do
     case $opt in
@@ -14,50 +14,49 @@ while getopts ":d:e" opt; do
         ;;
         e)
             printStep "Install extra software"
-            snap install --classic code
-            apt update \
-            && apt install \
+            sudo snap install --classic code
+            sudo apt update \
+            && sudo apt install \
                 micro \
                 git \
                 openssh-server \
-            && ufw allow ssh
+            && sudo ufw allow ssh
         ;;
     esac
 done
 
-apt update &&  apt install curl gnupg2 lsb-release -y
+sudo apt update && sudo apt install curl gnupg2 lsb-release -y
 
 printStep "Setup language"
 locale  # check for UTF-8
-apt update &&  apt install locales
-locale-gen en_US en_US.UTF-8
-update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 locale  # verify settings
 
 printStep "Setup repositories"
-
-apt install software-properties-common -y
+sudo apt install software-properties-common -y
 if [ $? -gt 0 ]; then
     exit 1;
 fi
-add-apt-repository universe
+sudo add-apt-repository universe
 
-curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" \
-    |  tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" \
+    | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-curl -sSL https://packages.osrfoundation.org/gazebo.gpg -o /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" \
-    |  tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo curl -sSL https://packages.osrfoundation.org/gazebo.gpg -o /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" \
+    | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 
 printStep "Install ros dev tools"
-apt update && apt install -y ros-dev-tools
+sudo apt update && sudo apt install -y ros-dev-tools
 if [ $? -gt 0 ]; then
     exit 1;
 fi
 printStep "Install ros ${distor} Desktop"
-apt update && apt install -y ros-${distro}-desktop-full
+sudo apt update && sudo  apt install -y ros-${distro}-desktop-full
 if [ $? -gt 0 ]; then
     exit 1;
 fi
@@ -72,16 +71,17 @@ if [ $? -gt 0 ]; then
 fi
 
 printStep "Init rosdep"
-rosdep init
+sudo rosdep init
+rosdep update
 
 printStep "Install colcon"
-apt update && apt install python3-colcon-common-extensions -y
+sudo apt update && sudo apt install python3-colcon-common-extensions -y
 
 printStep "Install basic packages"
-apt update &&  apt install \
+sudo apt update && sudo apt install \
     ros-${ROS_DISTRO}-joint-state-publisher \
     ros-${ROS_DISTRO}-joint-state-publisher-gui \
     ros-${ROS_DISTRO}-xacro \
     ros-${ROS_DISTRO}-ros-gazebo-ros-pkgs
 
-apt autoremove && apt clean
+sudo apt autoremove && sudo apt clean
