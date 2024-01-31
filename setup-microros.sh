d5ce3e2abb2b9fc4dcd0a7f0f32d208cec685e61
MHummel <mhummel@artiso.com>
2024-01-31 10:08:49 +0100

    add micro ros setup

---
+++ setup-microros.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source /opt/ros/$ROS_DISTRO/setup.bash
+
+mkdir ~/uros_ws && cd uros_ws
+
+git clone -b $ROS_DISTRO https://github.com/micro-ROS/micro_ros_setup.git src/micro_ros_setup
+
+rosdep update && rosdep install --from-paths src --ignore-src -y
+
+colcon build
+
+echo "source ~/uros_ws/install/local_setup.bash" >> ~/.bashrc
