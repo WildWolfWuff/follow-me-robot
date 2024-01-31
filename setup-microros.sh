f3476c4ad7d59e20646d29d7d242dacf2152058d
MHummel <mhummel@artiso.com>
2024-01-31 11:19:10 +0100

    configure vscode for this branch

--- setup-microros.sh
+++ setup-microros.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source /opt/ros/$ROS_DISTRO/setup.bash
 
-mkdir ~/uros_ws && cd uros_ws
+mkdir ~/uros_ws
+cd ~/uros_ws
 
 git clone -b $ROS_DISTRO https://github.com/micro-ROS/micro_ros_setup.git src/micro_ros_setup
 
@@ -10,3 +11,4 @@ rosdep update && rosdep install --from-paths src --ignore-src -y
 colcon build
 
 echo "source ~/uros_ws/install/local_setup.bash" >> ~/.bashrc
+source ~/uros_ws/install/local_setup.bash
