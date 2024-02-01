2fee504cb5c157c3775335168a6e9412c230d259
MHummel <mhummel@artiso.com>
2024-02-01 21:05:59 +0100

    improve robot controll

--- src/motor_controller/Mecanum_Wheels_Axis/Mecanum_Wheels_Axis.ino
+++ src/motor_controller/Mecanum_Wheels_Axis/Mecanum_Wheels_Axis.ino
@@ -1,13 +1,13 @@
 #include <AccelStepper.h>
 #include <SoftwareSerial.h>
-#include <Ramp.h>
+// #include <Ramp.h>
 
 /*
  * Object Ramp
  * Documentation regarding rhe Ramp.h
  * https://github.com/siteswapjuggler/RAMP
  */
-rampInt speedRamp;
+// rampInt speedRamp;
 
 //Object Bluetooth (RX,TX)
 //SoftwareSerial Bluetooth(A8, 38);
@@ -21,25 +21,12 @@ AccelStepper LeftFrontWheel(1, 2, 5);  // Stepper2 (x,step, dir)
 AccelStepper RightBackWheel(1, 10, 9); // Stepper3 (x,step, dir) (1, 44, 45)
 AccelStepper RightFrontWheel(1, 7, 8); // Stepper4 (x,step, dir) (1, 46, 47)
 
-bool control = false;
-bool startTestCode = false;
-bool alreadyStarted = false;
-bool testWithoutRamp = false;
-bool inside = true;
-int state = 0;
-int testControl = 0;
-int minRampVal = 105;
-int steps = 0;
-int commands = 0;
-int wheelSpeed = 500; //Initial speed value from 0 //previous value is 255
-int rampSpeed = 0;
-int minRampTime = 500;
-int rampTime = 2000; //1 second and 500 ms ramp as default
-char readSerial = 0;
-char ramainMovement = 's';
-unsigned long myTime;
-unsigned long newTimer;
-unsigned long generalTime;
+// input vector linear x,y and angular z 
+String x="0.0";
+String y="0.0";
+String z="0.0";
+
+int wheelSpeed = 5000; //Initial speed value from 0 //previous value is 255
 
 double wheel_distance_width=20;
 double wheel_distance_length=21;
@@ -49,36 +36,18 @@ double wheel_radius=7.5;
 
 void setMovement(float linearX, float linearY, float angularZ)
 {
+  // caluclates the movement of each wheel
+  // see http://robotsforroboticists.com/drive-kinematics/ for more informations
   double lf_wheel_speed = (1/wheel_radius) * (linearX - linearY - angularZ) * wheelSpeed;
   double rf_wheel_speed = (1/wheel_radius) * (linearX + linearY + angularZ) * wheelSpeed;
   double lb_wheel_speed = (1/wheel_radius) * (linearX + linearY - angularZ) * wheelSpeed;
   double rb_wheel_speed = (1/wheel_radius) * (linearX - linearY + angularZ) * wheelSpeed;
+  // sets the movement to the wheels
   LeftFrontWheel.setSpeed(-lf_wheel_speed);
   RightFrontWheel.setSpeed(rf_wheel_speed);
   LeftBackWheel.setSpeed(-lb_wheel_speed);
   RightBackWheel.setSpeed(rb_wheel_speed);
   log("x="+String(linearX)+" y="+String(linearY)+" z="+String(angularZ)+" lf="+String(lf_wheel_speed)+" rf="+String(rf_wheel_speed)+" lb="+String(lb_wheel_speed)+" rb="+String(rb_wheel_speed));
-}
-
-void stopMovement(){
-    LeftFrontWheel.setSpeed(0);
-    LeftBackWheel.setSpeed(0);
-    RightFrontWheel.setSpeed(0);
-    RightBackWheel.setSpeed(0);
-}
-
-void secondRampChangeDir() {
-  if (speedRamp.update() > 0 && steps == 0) {
-    // Serial.println("Ramp change dir step 1");
-    speedRamp.go(minRampVal, 500);
-    steps = 1;
-  }
-  if (speedRamp.update() == minRampVal && steps == 1 ) {
-    //change to new direction
-    speedRamp.go(wheelSpeed, rampTime);
-    steps = 0;
-    control = false;
-  }
 }
 
 void setup(){
@@ -99,10 +68,13 @@ void setup(){
     /*
     * Start the SpeedRamp as a initial value of 100
     */
-    speedRamp.go(100);
+    // speedRamp.go(100);
+    log("ready");
 }
 
-String getValue(String data, char separator, int index,String defaultValue)
+// Gets the sub string from a string based on a separator char and its index.
+// If index not exists it retuns a default value
+String getValue(String data, char separator, int index, String defaultValue)
 {
   int found = 0;
   int strIndex[] = {0, -1};
@@ -117,14 +89,10 @@ String getValue(String data, char separator, int index,String defaultValue)
   }
   return found>index ? data.substring(strIndex[0], strIndex[1]) : defaultValue;
 }
-String x="0.0";
-String y="0.0";
-String z="0.0";
 
+// logs a string to a Serial output for monitor the system
 void log(String text){
-  // if(Serial.available() > 0){
   Serial.println(text);
-  // }
 }
 
 void loop(){
@@ -140,17 +108,18 @@ void loop(){
     RightFrontWheel.runSpeed();
     LeftBackWheel.runSpeed();
     LeftFrontWheel.runSpeed();
+    /* 
+    * Recives the movement values from seral input.
+    * The recived string is split by ';' to get x,y and z values.
+    * This cordinates will be given in the set move function
+    */
     if(Serial.available() > 0){
       String vector = Serial.readStringUntil('\n');
       x = getValue(vector,';',0, "0.0");
       y = getValue(vector,';',1, "0.0");
       z = getValue(vector,';',2, "0.0");
-      log("x:"+x+";y:"+y+";z"+z);
       setMovement(x.toFloat(),y.toFloat(),z.toFloat());
     }
-    if(Serial1.available())
-    {
-      String speed= Serial1.readStringUntil('\n');
-      wheelSpeed=speed.toInt();
-    }
+    // TODO add speed regulation
+    // TODO add handler for connection errors to stop the robot
 }
--- src/motor_controller/readcontrolerinput.py
+++ src/motor_controller/readcontrolerinput.py
@@ -3,6 +3,7 @@ import math
 import threading
 import serial
 from time import sleep
+import logging
 
 class XboxController(object):
     MAX_TRIG_VAL = math.pow(2, 8)
@@ -101,29 +102,40 @@ class XboxController(object):
                     self.UpDPad = event.state
                 elif event.code == 'BTN_TRIGGER_HAPPY4':
                     self.DownDPad = event.state
+class SerialMonitor(object):
+    def __init__(self, ser):
+        self.ser=ser
+        self.display_thread=threading.Thread(target=self._print_msg,args=())
+        self.display_thread.daemon = True
+        self.display_thread.start()
+    def _print_msg(self):
+        while self.ser.open:
+            result = self.ser.read_until()
+            print(result.decode("ASCII"))
+            # logging.info(result.decode("ASCII"))
 
 if __name__ == '__main__':
     dec=1
     sep=';'
-    ser = serial.Serial('COM4',9600)
-    speed=100
+    ser = serial.Serial('COM5',9600)
+    speed=1
     try:
+        monitor=SerialMonitor(ser)
         joy = XboxController()
-        px=float(0.0)
-        py=float(0.0)
-        pz=float(0.0)
+        px=str('0.0')
+        py=str('0.0')
+        pz=str('0.0')
         while True:
             values=joy.read()
-            x=float(values[0][2])
-            y=float(values[0][0])
-            z=float(values[0][1])
+            x=str(round(values[0][1],dec) * speed)
+            y=str(-round(values[0][0],dec) * speed)
+            z=str(round(values[0][2],dec) * speed)
             if( x!=px or y!=py or z != pz):
-                print(values)
-                s=str(round(x*2.0, dec))+";"+str(round(y*3, dec))+";"+str(round(z*3, dec))+"\n"
-                ser.write(s.encode())
+                msg=x+";"+y+";"+z+"\n"
+                print(msg)
+                ser.write(msg.encode())
                 ser.flush()
-                # result = ser.read_until()
-                # print(result.decode("ASCII"))
+                # sleep(0.05)
             px=x
             py=y
             pz=z
