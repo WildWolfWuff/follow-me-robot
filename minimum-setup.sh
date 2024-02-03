2977f29643afa67f8601b5707edc036e8e56086e
MHummel <mhummel@artiso.com>
2024-02-03 14:39:33 +0100

    add setup script for testing

---
+++ minimum-setup.sh
@@ -0,0 +1,19 @@
+distro=iron
+sudo apt update && sudo apt install curl gnupg2 lsb-release -y
+sudo add-apt-repository universe
+
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" \
+    | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo curl -sSL https://packages.osrfoundation.org/gazebo.gpg -o /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" \
+    | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+printStep "Install ros ${distor} Desktop"
+sudo apt update && sudo  apt install -y ros-${distro}-desktop-full
+source /opt/ros/${distro}/setup.bash
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro ${iron} -r -y
+sudo apt update && sudo apt install python3-colcon-common-extensions -y
+colcon build
