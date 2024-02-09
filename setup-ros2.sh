6f8d67778c3c51af97af38bd4d1ac3bb1f5bc09e
Tobias703 <seiffertt.tin21@student.dhbw-heidenheim.de>
2024-02-09 11:40:02 +0100

    -y

--- setup-ros2.sh
+++ setup-ros2.sh
@@ -63,7 +63,7 @@ sudo apt install software-properties-common -y
 if [ $? -gt 0 ]; then
     exit 1;
 fi
-sudo add-apt-repository universe
+sudo add-apt-repository universe -y
 
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" \
