3ff6c83aef98c3e170969ec291ceea2849abc4be
Tobias703 <seiffertt.tin21@student.dhbw-heidenheim.de>
2024-08-06 12:51:29 +0200

    Wheel Controller fix

--- compile.bash
+++ compile.bash
@@ -8,4 +8,4 @@ fi
 setup_path="$PWD/install/setup.$used_shell"
 echo "impport: $setup_path"
 . $setup_path
-ros2 launch follow_me_robot robot.launch.py #start_sim:=true use_foxglove:=true
+ros2 launch follow_me_robot robot.launch.py start_sim:=true use_foxglove:=true
